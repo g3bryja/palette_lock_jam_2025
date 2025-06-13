19c477e46a63d7efc47a9be6681814babbc3ce8a
Madeline Bartley <madeline.bartley@outlook.com>
2025-06-13 18:38:29 -0400

    Added player controller using new input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Vector2 input;
+    private CharacterController characterController;
+    private Vector3 direction;
+
+    [SerializeField] private float speed;
+
+    private void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        characterController.Move(direction * speed * Time.deltaTime);
+
     }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+        direction = new Vector3(input.x, 0.0f, input.y);
+        Debug.Log("Loggin' like Paul Bunyun");
+    }
+
 }
